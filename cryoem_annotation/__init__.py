151b6f183e9db3241e555ef1ec8f9f224e668685
Felipe Munoz <felipemunoz8128@gmail.com>
2025-12-17 13:07:04 -0500

    Fix import error: normalize_image is in image_processing, not image_loader

--- cryoem_annotation/__init__.py
+++ cryoem_annotation/__init__.py
@@ -9,7 +9,8 @@ __version__ = "0.1.0"
 __author__ = "Cryo-EM Annotation Tool Contributors"
 
 from cryoem_annotation.core.sam_model import load_sam_model
-from cryoem_annotation.core.image_loader import load_micrograph, normalize_image
+from cryoem_annotation.core.image_loader import load_micrograph
+from cryoem_annotation.core.image_processing import normalize_image
 from cryoem_annotation.core.colors import generate_label_colors
 
 __all__ = [
